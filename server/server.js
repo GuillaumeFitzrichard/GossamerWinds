28456639d10226d356b8a3b4a50a19d4cd7d3e71
GuillaumeFitzrichard <alfredhwolfe@gmail.com>
2022-02-21 15:35:11 -0700

    test render

--- server/server.js
+++ server/server.js
@@ -12,6 +12,6 @@ const PORT = config.app.port;
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/../public')));
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-app.listen(PORT, () => console.log (`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
