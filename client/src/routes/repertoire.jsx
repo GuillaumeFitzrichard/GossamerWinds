fb8dc37a4c5fd5aba4e66b75166169c22b4f047d
GuillaumeFitzrichard <alfredhwolfe@gmail.com>
2022-02-27 19:00:25 -0700

    repertoire calculations

--- client/src/routes/repertoire.jsx
+++ client/src/routes/repertoire.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { 
+  getClassical,
+  getPopular,
+  getLatin,
+} from '../../dist/musics.js';
 
-function Repertoire() {
+let catalogue = (pieces) => {
+  let sortedCatalogue = {};
+  pieces.forEach((piece) => {
+    let splitOne = piece.indexOf(' - ');
+    let splitTwo = piece.lastIndexOf(' - ');
+    let composer = piece.slice(0, splitOne);
+    let movement = '';
+    let work = piece.slice(splitOne + 3, piece.length);
+    if (splitOne != splitTwo) {
+      movement = piece.slice(splitTwo + 3, piece.length);
+      work = piece.slice(splitOne + 3, splitTwo);
+    };
+    let composition = {
+      title: work,
+      movement: movement,
+    };
+    if (!sortedCatalogue[composer]) {
+      sortedCatalogue[composer] = {
+        compositions: []
+      }
+    }
+    sortedCatalogue[composer].compositions.push(composition)
+  })
+  
+  return sortedCatalogue
+}
 
+const Repertoire = () => {
+
+  
+  const getPieces = (style) => {
+ 
+  }
   return(
     <div id="styles">
+      <h1>Gossamer Winds' repertoire includes selections from a variety of styles, including:</h1>
       <h3>Classical</h3>
       <h3>Popular</h3>
       <h3>Latin</h3>
